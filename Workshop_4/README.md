c74bd3a5f045aa58977fb6a90ba866d3fd9c2825
CallumJHays <callumjhays@gmail.com>
2021-09-14 10:33:10 +1000

    Get slam dunking with the basketball working

--- Workshop_4/README.md
+++ Workshop_4/README.md
@@ -1,25 +1,50 @@
-# QUT ROS Workshop 4
+# QUT ROS Workshop 4: MoveIt! Slam DUNK
 
-## MoveIt! Slam Dunk
+![final product in rviz](./dunk.png "Slam Dunk!")
 
-Install deps and setup workspace:
+## Setup
 
 ```bash
 sudo apt install ros-melodic-moveit python-catkin-tools
 source /opt/ros/melodic/setup.sh
 catkin build
+```
+
+## Running The Script
+
+```bash
 source devel/setup.sh
+roslaunch slam_dunk_moveit_config demo.launch
+```
+
+And in another terminal:
+
+```bash
+source devel/setup.sh
+python3 slam_dunk.py
+```
+
+## Workshop Instructions
+
+Backup and remove the `slam_dunk_moveit_config` folder:
+
+```bash
+mv src/slam_dunk_moveit_config slam_dunk_moveit_config.bak
 ```
 
 Run MoveIt! Setup Assistant:
 
 ```bash
+source devel/setup.sh
 roslaunch moveit_setup_assistant setup_assistant.launch
-catkin build
 ```
 
-Run the demo launch and test setting target poses manually:
+TODO!: detailed setup_assistant instructions
+
+Run the demo launch and test moving the arm around in an empty environment:
 
 ```bash
-roslaunch
+roslaunch slam_dunk_moveit_config demo.launch
 ```
+
+TODO!: detailed slam_dunk.py coding instructions
--- Workshop_4/slam_dunk.py
+++ Workshop_4/slam_dunk.py
@@ -1,4 +1,6 @@
 import sys
+from typing import Iterable
+from moveit_commander import robot
 
 import rospy
 from std_msgs.msg import Header
@@ -9,75 +11,87 @@ from moveit_commander.move_group import MoveGroupCommander
 from moveit_commander.planning_scene_interface import PlanningSceneInterface
 
 moveit_commander.roscpp_initialize(sys.argv)
-rospy.init_node('slam_dunk')
+rospy.init_node('slam_dunk_node')
 
-ORIGIN = PoseStamped(
-    header=Header(
-        frame_id="world"
-    ),
-    pose=Pose(
-        position=Point(0, 0, 0),
-        orientation=Quaternion(0, 0, 0, 1)
+robot_arm = MoveGroupCommander('slam_dunk_planning_group')
+
+target_pose = rospy.Publisher(
+    "target_pose", PoseStamped, queue_size=10, latch=True)
+
+environment = PlanningSceneInterface(synchronous=True)
+
+
+def define_pose(position: Iterable[float], quaternion: Iterable[float]):
+    return PoseStamped(
+        header=Header(
+            frame_id="world"
+        ),
+        pose=Pose(
+            position=Point(*position),
+            orientation=Quaternion(*quaternion)
+        )
     )
+
+
+# define important poses wrt. the "world" frame
+ORIGIN_POSE = define_pose(
+    [0, 0, 0],
+    [0, 0, 0, 1]
+)
+BASKETBALL_POSE = define_pose(
+    [0.4468396053, -0.0034719435, 0.3678862186],
+    [0, 0, 0, 1]
+)
+BASKETBALL_GRASP_POSE = define_pose(
+    [0.3871681929, 0.0200828641, 0.4057507893],
+    [0.0843928720, 0.8555283434, -0.1217124127, 0.4961201321]
+)
+DUNK_IN_HOOP_POSE = define_pose(
+    [-0.3412968401, -0.0248166078, 0.3888232048],
+    [-0.4673231353, -0.8836860646, -0.0240147086, -0.0114595050]
 )
 
-environment = PlanningSceneInterface()
-# MUST SLEEP. PlanningSceneInterface's implementation requires this;
-# otherwise the add_mesh function below will be ignored.
-# Why? Your guess is as good as mine.
-rospy.sleep(2)
-environment.add_mesh('obstacles', ORIGIN, "environment.stl",
+environment.add_mesh('environment', ORIGIN_POSE, "environment.stl",
                      size=(0.01, 0.01, 0.01))
+environment.add_mesh('basketball', BASKETBALL_POSE, "basketball.stl",
+                     size=(0.2, 0.2, 0.2))
 
-robot_arm = MoveGroupCommander('slam_dunk')
-
-# our obstacle course is quite challenging! We need to give moveit a bit of extra time.
-# robot_arm.set_planning_time(20)
+# MoveIt computes trajectories somewhat non-deterministically.
+# Our obstacle course is quite challenging; so lets give it some more time to work with
+robot_arm.set_planning_time(10)
 robot_arm.set_num_planning_attempts(10)
 
-# just above the hoop
-HOOP = PoseStamped(
-    header=Header(
-        frame_id="world"
-    ),
-    pose=Pose(
-        position=Point(-0.3475245233, -0.0232578443, 0.3682473160),
-        orientation=Quaternion(-0.9986380939, -0.0345536632,
-                               -0.0390896358, 0.0000472383)
-    )
-)
-BASKETBALL = PoseStamped(
-    header=Header(
-        frame_id="world"
-    ),
-    pose=Pose(
-        position=Point(0.4468396053, -0.0034719435, 0.3728862186),
-        orientation=Quaternion(-0.9982654178, 0.0565203398,
-                               0.0164763244, 0.0003709629)
-    )
-)
 
-targets = rospy.Publisher("targets", PoseStamped, queue_size=10)
 rospy.sleep(0.5)  # sleep so targets.publish() works
 
-targets.publish(BASKETBALL)
-robot_arm.set_pose_target(BASKETBALL.pose)
-success, plan, _, _ = robot_arm.plan()
-assert success
 
-# now execute the planned path.
-# This should always succeed as our robot is simulated and will do exactly what we command!
-success = robot_arm.execute(plan, wait=True)
-assert success
+def move_to_pose(pose: PoseStamped):
+    # publish the goal so that we can see it in rviz
+    target_pose.publish(pose)
 
-targets.publish(HOOP)
-robot_arm.set_pose_target(HOOP.pose)
-success, plan, _, _ = robot_arm.plan()
-assert success
+    # calculate a path that could be followed to reach the
+    robot_arm.set_pose_target(pose.pose)
+    success, plan, _, _ = robot_arm.plan()
+    assert success
 
-# now execute the planned path.
-# This should always succeed as our robot is simulated and will do exactly what we command!
-success = robot_arm.execute(plan, wait=True)
-assert success
+    # now execute the planned path.
+    # This should always succeed as our robot is simulated and will do exactly what we command!
+    success = robot_arm.execute(plan, wait=True)
+    assert success
+
+
+print("Moving to BASKETBALL_GRASP_POSE")
+move_to_pose(BASKETBALL_GRASP_POSE)
+
+robot_arm.attach_object('basketball')
+
+try:
+    print("Moving to SLAM DUNK")
+    move_to_pose(DUNK_IN_HOOP_POSE)
+
+finally:
+    robot_arm.detach_object('basketball')
+
+print("MJ got nothin' on me")
 
 moveit_commander.roscpp_shutdown()
